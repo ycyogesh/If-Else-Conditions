f32b685581997fb346d9b5f02d78755ad9ba74bc
Yogeshwaran C <84272111+ycyogesh@users.noreply.github.com>
2022-07-24 12:37:27 +0530

    Triangle Validation using Angles

---
+++ Triangle validation.py
@@ -0,0 +1,12 @@
+a = int(input("Enter Angle 1: "))
+b = int(input("Enter Angle 2: "))
+c = int(input("Enter Angle 3: "))
+
+"""
+   a+b+c=180'
+"""
+
+if(a+b+c==180):
+    print("Valid")
+else:
+    print("Not Valid")
