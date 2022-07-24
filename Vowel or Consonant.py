a5aa340fc760d0c306191da9bc2e8ffefbd8059b
Yogeshwaran C <84272111+ycyogesh@users.noreply.github.com>
2022-07-24 12:12:03 +0530

    Charcter is Vowel or Consonant

---
+++ Vowel or Consonant.py
@@ -0,0 +1,8 @@
+a = input("Enter a Character : ")
+
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u' or a=='A' or a=='E' or a=='I' or a=='O' or a=='U'):
+    print(a," is a Vowel")
+elif(a>='0' and a<='9'):
+    print("Not a Vowel Enter a Character")
+else:
+    print("Consonant")
