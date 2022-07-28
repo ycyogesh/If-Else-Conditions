b5f010fa57467bb1b24c606b8e3d700014b1aa8a
Yogeshwaran C <84272111+ycyogesh@users.noreply.github.com>
2022-07-28 12:54:25 +0530

    Find week day using week number

---
+++ Week Day & Week Number.py
@@ -0,0 +1,20 @@
+#Week day and week number
+
+a = input("Enter Week Number : ")
+
+if(a=='1'):
+    print("Sunday")
+elif(a=='2'):
+    print("Monday")
+elif(a=='3'):
+    print("Tuesday")
+elif(a=='4'):
+    print("Wednesday")
+elif(a=='5'):
+    print("Thursday")
+elif(a=='6'):
+    print("Friday")
+elif(a=='7'):
+    print("Saturday")
+else:
+    print("Enter Correct Week Number(1-7)")
